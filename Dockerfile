f798a1db60226309760b65f179f44e6b9b3c5f55
Colin Cromar <colincromar@gmail.com>
2025-03-04 13:20:57 -0700

    echo server

---
+++ Dockerfile
@@ -0,0 +1,59 @@
+ARG BUILDER_IMAGE="elixir:1.18-slim"
+ARG RUNNER_IMAGE="debian:bookworm-slim"
+
+FROM ${BUILDER_IMAGE} AS builder
+
+# Set env variables
+ENV MIX_ENV="prod"
+
+# Install build dependencies
+RUN apt-get update -y && apt-get install -y build-essential git \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+
+# Install Hex and rebar3
+RUN mix do local.hex --force, local.rebar --force
+
+# Copy configuration
+# COPY config config
+
+# Copy mix files
+COPY mix.exs mix.exs
+COPY mix.lock mix.lock
+
+# Install dependencies
+RUN mix do deps.get --only $MIX_ENV, deps.compile
+
+# Copy application source code
+COPY lib lib
+
+# Compile the project
+RUN mix compile
+
+# Copy release configuration and build the release
+# COPY rel rel
+RUN mix release
+COPY rel rel
+
+## Runner image
+
+FROM ${RUNNER_IMAGE}
+
+# Install runtime dependencies
+RUN apt-get update -y && apt-get install -y libstdc++6 openssl libncurses5 locales \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Set the locale
+RUN sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen && locale-gen
+ENV LANG en_US.UTF-8
+ENV LANGUAGE en_US:en
+ENV LC_ALL en_US.UTF-8
+
+WORKDIR /app
+
+# Copy the compiled release from the builder stage
+COPY --from=builder /app/_build/prod/rel /app
+
+# Start the application
+CMD /app/protohackers/bin/protohackers start
---
+++ lib/protohackers/echo_server.ex
@@ -0,0 +1,70 @@
+defmodule Protohackers.EchoServer do
+  use GenServer
+
+  require Logger
+
+  @spec start_link(keyword()) :: GenServer.on_start()
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts)
+  end
+
+  defstruct [:listen_socket, :supervisor]
+
+  @impl true
+  def init(opts) do
+    port = Keyword.fetch!(opts, :port)
+    {:ok, supervisor} = Task.Supervisor.start_link(max_children: 100)
+
+    listen_options = [
+      ifaddr: {0, 0, 0, 0},
+      mode: :binary,
+      active: false,
+      reuseaddr: true,
+      exit_on_close: false
+    ]
+
+    case :gen_tcp.listen(port, listen_options) do
+      {:ok, listen_socket} ->
+        Logger.info("Started server on port #{port}")
+        state = %__MODULE__{listen_socket: listen_socket, supervisor: supervisor}
+        {:ok, state, {:continue, :accept}}
+
+      {:error, reason} ->
+        {:stop, reason}
+    end
+  end
+
+  @impl true
+  def handle_continue(:accept, %__MODULE__{} = state) do
+    case :gen_tcp.accept(state.listen_socket) do
+      {:ok, socket} ->
+        Task.Supervisor.start_child(state.supervisor, fn -> handle_connection(socket) end)
+        {:noreply, state, {:continue, :accept}}
+
+      {:error, reason} ->
+        {:stop, reason}
+    end
+  end
+
+  ## Helpers
+
+  defp handle_connection(socket) do
+    case recv_until_closed(socket, _buffer = "", _buffered_size = 0) do
+      {:ok, data} -> :gen_tcp.send(socket, data)
+      {:error, reason} -> Logger.error("Failed to receive data: #{inspect(reason)}")
+    end
+
+    :gen_tcp.close(socket)
+  end
+
+  @limit _100_kb = 1024 * 100
+
+  defp recv_until_closed(socket, buffer, buffered_size) do
+    case :gen_tcp.recv(socket, 0, 10_000) do
+      {:ok, data} when buffered_size + byte_size(data) > @limit -> {:error, :buffer_overflow}
+      {:ok, data} -> recv_until_closed(socket, [buffer, data], buffered_size + byte_size(data))
+      {:error, :closed} -> {:ok, buffer}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+end
